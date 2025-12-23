df508517d8cab5c6b0d090a44d7c15c56944502d
asakueqq <gurzijdaniil@gmail.com>
2025-12-23 23:06:59 +0000

    02:06:27.338 Read more: https://nextjs.org/docs/messages/invalid-use-se

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -2,5 +2,12 @@
 
 import {ai} from '@/ai/genkit';
 import '@/ai/flows/suggest-search-areas-for-lost-hiker';
+import {NextRequest} from 'next/server';
 
-export {ai as GET, ai as POST};
+export async function GET(req: NextRequest) {
+  return await ai.handleRequest(req);
+}
+
+export async function POST(req: NextRequest) {
+  return await ai.handleRequest(req);
+}
